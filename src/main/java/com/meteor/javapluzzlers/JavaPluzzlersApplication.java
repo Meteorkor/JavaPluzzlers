ba4d9814e06febbd678e50f254dc10ed94e89e57
unseok.kim <unseok.kim@linecorp.com>
2022-06-27 21:56:26 +0900

    init proj

---
+++ src/main/java/com/meteor/javapluzzlers/JavaPluzzlersApplication.java
@@ -0,0 +1,13 @@
+package com.meteor.javapluzzlers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPluzzlersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaPluzzlersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/meteor/javapluzzlers/JavaPluzzlersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meteor.javapluzzlers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPluzzlersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
